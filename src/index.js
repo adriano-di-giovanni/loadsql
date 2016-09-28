5f72cb97600fc621fc0b4898ad361885affe9ce7
Adriano Di Giovanni <me@adrianodigiovanni.com>
2016-09-28 16:52:06 +0200

    add caching

--- src/index.js
+++ src/index.js
@@ -5,23 +5,48 @@ export default class SQLLoader {
   constructor (basePath = './', ext = '.sql') {
     this.basePath = path.resolve(basePath)
     this.ext = ext
+    this.cache = {}
   }
 
   load (filepath, callback) {
+    const { cache } = this
     const fullpath = this._getFullPath(filepath)
     if (callback) {
-      return fs.readFile(fullpath, 'utf8', callback)
+      if (cache[fullpath]) {
+        callback(null, cache[fullpath])
+        return void 0
+      }
+      return fs.readFile(fullpath, 'utf8', (error, data) => {
+        if (error) {
+          callback(error)
+          return void 0
+        }
+        cache[fullpath] = data
+        callback(null, data)
+      })
     }
     return new SQLLoader.Promise((resolve, reject) => {
+      if (cache[fullpath]) {
+        return resolve(cache[fullpath])
+      }
       return fs.readFile(fullpath, 'utf8', (error, data) => {
-        error ? reject(error) : resolve(data)
+        if (error) {
+          reject(error)
+          return void 0
+        }
+        cache[fullpath] = data
+        resolve(data)
       })
     })
   }
 
   loadSync (filepath) {
+    const { cache } = this
     const fullpath = this._getFullPath(filepath)
-    return fs.readFileSync(fullpath, 'utf8')
+    if (!cache[fullpath]) {
+      cache[fullpath] = fs.readFileSync(fullpath, 'utf8')
+    }
+    return cache[fullpath]
   }
 
   _normalizePath (filepath) {
